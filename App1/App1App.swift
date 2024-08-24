16a5b04fc94a14448947c2f0aa5084c7f3b1335b
quanhoang55 <quanhoang1026@gmail.com>
2024-08-24 07:17:16 +0700

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Quan Hoang on 24/8/24.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Quan Hoang on 24/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
